28170f143b62b3eff0b5cc40b2d0f7990c95b12f
eliasbar <elias.barrado.jimenez@es.ibm.com>
2018-04-24 18:41:52 +0200

    added time

--- gen_block.c
+++ gen_block.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include <string.h>
 #include <time.h>
-//#include <ctype.h>
 #include <math.h>
 #include <stdint.h>
-//#include <unistd.h>
+
 
 #define RR(x, n) (((x) >> (n)) | ((x) << (32-(n))))
 #define RL(x, n) (((x) << (n)) | ((x) >> (32-(n))))
@@ -53,6 +51,7 @@ const uint32_t mask_bit[33] = { 0x0,
                                 0x01000000,0x02000000,0x04000000,0x08000000,0x10000000,0x20000000,0x40000000,0x80000000 };
 
 
+// Prints binary representation of uint_32_t
 void print_bin(uint32_t hex){
 	int bin[32];
 	for (int i=32; i>0; i--) {
@@ -78,21 +77,56 @@ void print_bin(uint32_t hex){
 }
 
 
-//uint32_t X;
+// Prints ∆Q[i] = (X'[i] - X[i]) for i = 0.....31
+// Only for debug
+void printf_bsdr(uint32_t a, uint32_t b){
+	int a_bin[32], b_bin[32];
+	for (int i=32; i>0; i--) {
+		if ( (a & mask_bit[i]) == mask_bit[i] ) {
+			a_bin[i-1] = 1;
+
+		} else {
+			a_bin[i-1] = 0;
+		}
+
+		if ( (b & mask_bit[i]) == mask_bit[i] ) {
+			b_bin[i-1] = 1;
+
+		} else {
+			b_bin[i-1] = 0;
+		}
+	}
+
+	for (int i=0; i<32; i++){
+		if( a_bin[i] - b_bin[i] == 1 ){
+			printf("%d ",i);
+		}
+
+		if( a_bin[i] - b_bin[i] == -1 ){
+			if(i==0){
+				printf("-0 ");
+			}else{
+				printf("%d ", -i);
+			}
+		}
+
+	}
+	printf("\n");
+}
+
+// Random number generator
 uint32_t seed32_1, seed32_2; 
 uint32_t frandom() {
-  //X = (1103515245*X + 12345) & 0xffffffff;
-  //return X;
   uint32_t t = seed32_1 ^ (seed32_1 << 10);
   seed32_1 = seed32_2;
   seed32_2 = (seed32_2^(seed32_2>>10))^(t^(t>>13));
   return seed32_1;
 }
 
-/*Returns the b-th bit of a
- if a = 0x4000001 
- 	bit(a, 1) returns 1
- 	bit(a,31) returns 1	*/
+/*Returns the b-th bit of a 
+	For example if a = 0x4000001 
+		bit(a, 1) returns 1
+ 		bit(a,31) returns 1			*/
 uint32_t bit(uint32_t a, uint32_t b) {
     if ((b==0) || (b > 32)) 
       return 0;
@@ -125,18 +159,15 @@ int block(void){
   	uint64_t i = 0;
 	while(1){
 
+		if(i%10000000 == 0){
+			printf("%ld iterations\n", i);
+		}
+		i = i + 1;
+
 		
 		// Q[1]  = .... .... .... .... .... .... .... .... 
 	    Q [1]  = frandom();
-	    Q1[1]  = Q[1];
-	    	    
-	    /*
-	    if(i%1000000 == 0){
-			printf("%ld\n", i);
-			//printf("%08x\n",Q[1]);
-		}
-		*/
-		i = i + 1;
+	    Q1[1]  = Q[1];    
 
 	    // Q[2]  = .... .... .... .... .... .... .... .... 
 	    Q [2]  = frandom();
@@ -150,22 +181,15 @@ int block(void){
 	    Q [4]  = (frandom() & 0x7f00007f) | 0x80080800 | (Q[3] & 0x0077f780);
 	    Q1[4]  = Q[4];
 
-	    //  Q[5]  = 1... 1.0. 01.. 0000 0000 0000 001. .1.1
+	    //  Q[5]  = 1... 1.0. 0100 0000 0000 0000 001. .1.1
 	    // ∆Q[5]  = 0000 0000 0-++ ++++ ++++ ++++ ++00 0000
-	    Q [5]  = (frandom() & 0x7530001a) | 0x88400025;
+	    Q [5]  = (frandom() & 0x7500001a) | 0x88400025;
 	    Q1[5]  = Q[5] ^ 0x007fffc0;
 
 	    //  Q[6]  = 0000 001^ 0111 1111 1011 1100 0100 ^0^1
 	    // ∆Q[6]  = +000 0000 +000 0000 0000 0000 0-00 0000
 	    Q [6]  = 0x027fbc41 | (Q[5] & 0x0100000a);
 	    Q1[6]  = Q[6] ^ 0x80800040;
-
-	    /*
-	    if( (F(Q[ 6], Q[ 5], Q[ 4]) ^ F(Q1[ 6], Q1[ 5], Q1[ 4])) != 0x00004400 ){
-			continue;
-		}
-		*/
-
 
 	    //  Q[7]  = 0000 0011 1111 1110 1111 1000 0010 0000 
 	    // ∆Q[7]  = ++++ ++-- -000 0000 0000 -+++ ++-+ ++++
@@ -185,7 +209,7 @@ int block(void){
 	    //  Q[10] = 01.. .... 0..1 1111 1101 ...0 01.. ..00 
 	    // ∆Q[10] = +000 0000 0000 0000 00+- 0000 0000 0000
 	    Q [10] = (frandom() & 0x3f600e3c) | 0x401fd040;
-	    Q1[10] = Q[10] ^ 0x10003000;
+	    Q1[10] = Q[10] ^ 0x80003000;
 
 	    //  Q[11] = 00.. .... .... 0001 1100 ...0 11.. ..10 
 	    // ∆Q[11] = ++00 0000 0000 0000 0000 0000 0000 0000
@@ -205,7 +229,7 @@ int block(void){
 	    //  Q[14] = 0.0. ..00 .... 1011 111. ...1 1... 1... 
 	    // ∆Q[14] = +000 0000 0000 0000 0000 0000 0000 0000
 	    Q [14] = (frandom() & 0x5cf01e77) | 0x000be188;
-	    Q1[14] = Q[14] ^ 0xf0000000;
+	    Q1[14] = Q[14] ^ 0x80000000;
 
 	    //  Q[15] = 0.1. ..01 .... .... 1... .... .... 0... 
 	    // ∆Q[15] = +000 0000 0000 0000 -000 0000 0000 +000
@@ -228,7 +252,7 @@ int block(void){
 	    m[ 7] = RR(Q[ 8] - Q[ 7], 22) - F(Q[ 7], Q[ 6], Q[ 5]) - Q[ 4] - 0xfd469501;
 	    m[ 8] = RR(Q[ 9] - Q[ 8],  7) - F(Q[ 8], Q[ 7], Q[ 6]) - Q[ 5] - 0x698098d8;
 	    m[ 9] = RR(Q[10] - Q[ 9], 12) - F(Q[ 9], Q[ 8], Q[ 7]) - Q[ 6] - 0x8b44f7af;
-	    m[10] = RR(Q[11] - Q[10], 17) - F(Q[10], Q[ 9], Q[ 8]) - Q[ 7] - 0xffff5bb1; 
+	    m[10] = RR(Q[11] - Q[10], 17) - F(Q[10], Q[ 9], Q[ 8]) - Q[ 7] - 0xffff5bb1;
 	    m[11] = RR(Q[12] - Q[11], 22) - F(Q[11], Q[10], Q[ 9]) - Q[ 8] - 0x895cd7be;
 	    m[12] = RR(Q[13] - Q[12],  7) - F(Q[12], Q[11], Q[10]) - Q[ 9] - 0x6b901122;
 	    m[13] = RR(Q[14] - Q[13], 12) - F(Q[13], Q[12], Q[11]) - Q[10] - 0xfd987193;
@@ -253,125 +277,151 @@ int block(void){
 	    m1[14] = RR(Q1[15] - Q1[14], 17) - F(Q1[14], Q1[13], Q1[12]) - Q1[11] - 0xa679438e;
 	    m1[15] = RR(Q1[16] - Q1[15], 22) - F(Q1[15], Q1[14], Q1[13]) - Q1[12] - 0x49b40821; 
 
-	    if(DEBUG){
-	    	for(int i = 1; i < 16; i++){
-	    	print_bin(Q[i]);
-	    	print_bin(Q1[i]);
-	    	printf("\n");
-	    	}
-		}
 
-		return 0;
-
-	    if( (m[4] - m1[4]) != 0x80000000)
+	    if( (m1[4] - m[4]) != 0x80000000)
 	    	continue;
 	    
-	    if( (m[5] - m1[5]) != 0x00000000)
+	    if( (m1[5] - m[5]) != 0x00000000)
 	    	continue;
 
-	    if( (m[6] - m1[6]) != 0x00000000)
+	    if( (m1[6] - m[6]) != 0x00000000)
 	    	continue;
 	    
-	    if( (m[7] - m1[7]) != 0x00000000)
+	    if( (m1[7] - m[7]) != 0x00000000)
 	    	continue;
 
-	    if( (m[8] - m1[8]) != 0x00000000)
+	    if( (m1[8] - m[8]) != 0x00000000)
 	    	continue;
 	    
-	    if( (m[9] - m1[9]) != 0x00000000)
-	    	printf("%08x\n"  , ~(m[9] - m1[9]));
-	    	printf("%ld\n",i);
-	    	//return 0;
+	    if( (m1[9] - m[9]) != 0x00000000)
 	    	continue;
+
+	    if( (m1[10] - m[10]) != 0x00000000)
+	    	continue;
+
+	    if( (m1[11] - m[11]) != 0x00008000)
+	    	continue;
+		
+		if( (m1[12] - m[12]) != 0x00000000)
+	    	continue;
+
+	    if( (m1[13] - m[13]) != 0x00000000)
+	    	continue;
+
+	    if( (m1[14] - m[14]) != 0x80000000)
+	    	continue;
+
+	    if( (m1[15] - m[15]) != 0x00000000)
+	    	continue;
+	    
 	    /*
-	    if( (m[10] ^ m1[10]) != 0x00000000)
-	    	continue;
-		*/
-
-
-	    
-
 	    uint32_t temp;
 	    for( int k = 3; k<9 ; k++){
 	    	temp = F(Q1[k], Q1[k-1], Q1[k-2]) - F(Q[k], Q[k-1], Q[k-2]);
 	    	printf("%2.2d %08x\n", k, temp);
 	    }
 
+	    */
 
-	    
-	    for(int j = 0 ; j<16; j++){
-	    	printf("%2.2i %08x\n"  , j, m [j]);
-	    	printf("%2.2i %08x\n\n", j, m1[j]);
-	    }
-		
-	    return 0;
 
 	    // Q[17] = 0... .... .... ..0. ^... .... .... ^... 
-	    Q[17] = GG(Q[13], Q[16], Q[15], Q[14], m[ 1],  5, 0xf61e2562);
+	    Q [17] = GG(Q [13], Q [16], Q [15], Q [14], m [ 1],  5, 0xf61e2562);
+	    Q1[17] = GG(Q1[13], Q1[16], Q1[15], Q1[14], m1[ 1],  5, 0xf61e2562);
 	    if ( bit(Q[17],32) || bit(Q[17],18) || bit(Q[17],16) != bit(Q[16],16) || bit(Q[17],4) != bit(Q[16],4) )
 	    	continue;
 	    
 
 
 		// Q[18] = 0.^. .... .... ..1. .... .... .... ....
-	    Q[18] = GG(Q[14], Q[17], Q[16], Q[15], m[ 6],  9, 0xc040b340);
+	    Q [18] = GG(Q [14], Q [17], Q [16], Q [15], m [ 6],  9, 0xc040b340);
+	    Q1[18] = GG(Q1[14], Q1[17], Q1[16], Q1[15], m1[ 6],  9, 0xc040b340);
 	    if ( bit(Q[18],32) || bit(Q[18],30) != bit(Q[17],30) || !bit(Q[18],18) )
 	    	continue;
 	    
 
 	    
 	    // Q[19] = 0... .... .... ..0. .... .... .... ....
-	    Q[19] = GG(Q[15], Q[18], Q[17], Q[16], m[11], 14, 0x265e5a51);
+	    Q [19] = GG(Q [15], Q [18], Q [17], Q [16], m [11], 14, 0x265e5a51);
+	    Q1[19] = GG(Q1[15], Q1[18], Q1[17], Q1[16], m1[11], 14, 0x265e5a51);
 	    if ( bit(Q[19],32) || bit(Q[19],18) )
 	    	continue;
 	    
 
 	    // Q[20] = 0... .... .... .... .... .... .... ....
-	    Q[20] = GG(Q[16], Q[19], Q[18], Q[17], m[ 0], 20, 0xe9b6c7aa); 
+	    Q [20] = GG(Q [16], Q [19], Q [18], Q [17], m [ 0], 20, 0xe9b6c7aa);
+	    Q1[20] = GG(Q1[16], Q1[19], Q1[18], Q1[17], m1[ 0], 20, 0xe9b6c7aa); 
 	    if ( bit(Q[20],32) )
 	    	continue;
 
 	    // Q[21] = 0... .... .... ..^. .... .... .... ....
-	    Q[21] = GG(Q[17], Q[20], Q[19], Q[18], m[ 5],  5, 0xd62f105d); 
+	    Q [21] = GG(Q [17], Q [20], Q [19], Q [18], m [ 5],  5, 0xd62f105d);
+	    Q1[21] = GG(Q1[17], Q1[20], Q1[19], Q1[18], m1[ 5],  5, 0xd62f105d);  
 	    if ( bit(Q[21],32) || bit(Q[21],18) != bit(Q[20],18) )
 	    	continue;
 
 	    // Q[22] = 0... .... .... .... .... .... .... ....
-	    Q[22] = GG(Q[18], Q[21], Q[20], Q[19], m[10],  9, 0x2441453); 
+	    Q [22] = GG(Q [18], Q [21], Q [20], Q [19], m [10],  9, 0x2441453);
+	    Q1[22] = GG(Q1[18], Q1[21], Q1[20], Q1[19], m1[10],  9, 0x2441453);  
 	    if ( bit(Q[22],32) )
 	    	continue;
 
 	    // Q[23] = 0... .... .... .... .... .... .... ....
-	    Q[23] = GG(Q[19], Q[22], Q[21], Q[20], m[15], 14, 0xd8a1e681); 
+	    Q [23] = GG(Q [19], Q [22], Q [21], Q [20], m [15], 14, 0xd8a1e681);
+	    Q1[23] = GG(Q1[19], Q1[22], Q1[21], Q1[20], m1[15], 14, 0xd8a1e681); 
 	    if ( bit(Q[23],32) )
 	    	continue;
 
 	    // Q[24] = 0... .... .... .... .... .... .... ....
-	    Q[24] = GG(Q[20], Q[23], Q[22], Q[21], m[ 4], 20, 0xe7d3fbc8); 
+	    Q [24] = GG(Q [20], Q [23], Q [22], Q [21], m [ 4], 20, 0xe7d3fbc8);
+	    Q1[24] = GG(Q1[20], Q1[23], Q1[22], Q1[21], m1[ 4], 20, 0xe7d3fbc8); 
 	    if ( !bit(Q[24],32) )
 	    	continue;
 
-	    // No conditions for Q[25]...Q[47
+	    // No conditions for Q[25]...Q[47]
 
+	    if(DEBUG){
+	    	printf("PRINTING BSDR: ∆Q[i]: \n");
+	    	for(int i = 1; i < 25; i++){
+	    		printf("%2.2d  ",i);
+	    		printf_bsdr(Q1[i],Q[i]);
+	    	}
+	    	printf("\n");
+	    }
 
+	    if(DEBUG){
+	    	printf("PRINTING Q[i], Q1[i]:\n");
+	    	for(int i = 1; i < 25; i++){
+	    	printf("%2.2d  ",i);
+	    	print_bin(Q[i]);
+	    	printf("%2.2d  ",i);
+	    	print_bin(Q1[i]);
+	    	printf("\n");
+	    	}
+		}
 
-
-	    printf("Found ya\n");
+	    printf("Found it in %ld iterations\n", i);
+	    printf("Found it in 2^%2.2lf iterations\n", log(i)/log(2));
 	    return 0;
 
 	}
 }
 
 int main (void){
+
+	clock_t t;
+    t = clock();
+	
+	// Initialicing seed for random number generator
 	srand ( time(NULL) );
-	//X = rand();
 	seed32_1 = rand();
-	seed32_2 = rand() % 1492637;
+	seed32_2 = rand() % 162287;
+
 	block();
-	uint32_t b = 0x80000002;
-	uint32_t a = bit(b,31);
-	int c;
-	c = (a == 0);
+
+	t = clock() - t;
+	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	printf("%f seconds to execute \n", time_taken);
+
 	return 0;
 }
 
